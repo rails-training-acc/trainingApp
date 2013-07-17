85f7da017cdcf03cd0a59b3dbb1c613905aa581f
Asja <sergeenkoanastasija@gmail.com>
2013-07-17 15:19:06 +0300

    User model added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,39 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+class ComplexityValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value) 
+    minSuccess  = 3 || options[:minSuccess]
+    rulesPassed = 0
+    special = "?<>',?[]}{=-)(_+*&^%@!$#`~{}"
+    regex_special = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+    regex_numeric = /[0-9]/
+    regex_small = /[a-z]/
+    regex_capitals = /[A-Z]/
+
+    if value =~ regex_special 
+      rulesPassed += 1
+    end
+    if value =~ regex_numeric 
+      rulesPassed  += 1
+    end
+    if value =~ regex_small 
+    rulesPassed += 1
+    end
+    if value =~ regex_capitals 
+      rulesPassed += 1
+    end
+
+    if (rulesPassed < minSuccess) 
+       record.errors.add attribute, (options[:message] || "is not strong password")
+    end
+  end
+end
 
 class User < ActiveRecord::Base
   has_secure_password
+
+  include ActiveModel::Validations
   attr_accessible :email, :name, :password, :password_confirmation
 
   validates :name, presence: true, length: { maximum: 32}
@@ -19,7 +49,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: EMAIL_REGEXP },
       uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 } 
+  validates :password, presence: true, length: { minimum: 6 }, complexity: true 
   validates_confirmation_of :password
 
   validates :password_confirmation, presence: true
