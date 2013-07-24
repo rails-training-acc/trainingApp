cdda8ed62d3d68af7bffd8ad250341a2fba96902
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2013-07-24 15:48:34 +0300

    FactoryGirl DEMO

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,14 @@ FactoryGirl.define do
   factory :user do
     name     "John Doe"
     email    "john@doe.com"
-    password "foobar"
-    password_confirmation "foobar"
+    password "foobaR123"
+    password_confirmation "foobaR123"
+  end
+
+  factory :admin_user do
+    name "John Doe Sr."
+    email "john.dow@example.com"
+    password "Foobar321"
+    password_confirmation "Foobar321"
   end
 end
